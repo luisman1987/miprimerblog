93f7c468ba5e293811af775490b755cfd5cb42e5
Luis Lopez <luis.ma@mesoamericana.edu.gt>
2016-09-23 08:59:37 -0700

    Se creo una conexion de datos y la plantilla

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from blog.models import Postear
 
 # Create your views here.
 def listar_articulo(request):
-    return render (request,'blog/listar_articulo.html',{})
+    articulo = Postear.objects.all()
+    return render (request,'blog/listar_articulo.html',{'articulo':articulo})
