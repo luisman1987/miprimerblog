e9ebb8ae0e249844a351357e89c75074780eafed
Luis Lopez <luis.ma@mesoamericana.edu.gt>
2016-09-30 08:41:54 -0700

    plantillas para cada articulo

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.listar_articulo),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.detalle_articulo),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from blog.models import Postear
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def listar_articulo(request):
     articulo = Postear.objects.all()
     return render (request,'blog/listar_articulo.html',{'articulo':articulo})
+
+def detalle_articulo(request, pk):
+    post = get_object_or_404(Postear, pk=pk)
+    return render(request, 'blog/detalle_articulo.html', {'post': post})
